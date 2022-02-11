3752de76e5f5b7d52f59f690482557cb7e55e947
Dustin Morgan <dustin.morgan@omes.ok.gov>
2022-02-11 14:04:38 -0600

    Added test suites to cover various site pages and sub-pages

---
+++ Test Suites/Program and Projects.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Program and Projects</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>9f92d972-09c5-4c2c-b06c-ed3f69657ca2</testSuiteGuid>
+</TestSuiteEntity>
